a3ca793c3b8472fcc74bc2a57100f95aefad7361
Andrzakourcev <144477949+Andrzakourcev@users.noreply.github.com>
2023-12-25 21:30:48 +0300

    Update genres.py

--- data/genres.py
+++ data/genres.py
@@ -1,13 +1,13 @@
-import sqlalchemy
+from sqlalchemy import Integer, String, Column
 from sqlalchemy import orm
-from .db_session import SqlAlchemyBase
+from data.db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 
 
 class Genre(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'genres'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    id = Column(Integer,
+                primary_key=True, autoincrement=True)
+    title = Column(String, nullable=True)
     book = orm.relationship("Book", back_populates='genre')
